f73dd60476ba5d8f039c8305f7abe0d49155d78c
Jonathan Cerritos <alancomando16@gmail.com>
2025-12-04 00:12:26 -0600

    Funciones de estado agregadas

---
+++ main.c
@@ -0,0 +1,199 @@
+/**
+ * @file main.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa de nuevo
+ * @version 0.1
+ * @date 2025-12-01
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include "graficos.h"
+#include <stdlib.h>
+#include <time.h>
+#define GRAVEDAD 1
+#define ALTURA_DEL_PASTO 600
+#define VELOCIDAD_CAIDA 8
+#define MIN_Y_APARICION 50
+#define MAX_Y_APARICION 300
+
+typedef struct
+{
+    int coorX;
+    int coorY;
+    int estado;
+    int alto;
+    int ancho;
+    int velX;
+    int velY;
+} Pato;
+
+void dibujarEscenarioRes1(Imagen *piso, Imagen *arbol);
+void dibujarEscenarioRes2(Imagen *piso, Imagen *arbol);
+void dibujarPato(Imagen *patoImg, Pato *pato);
+void vueloPato(Pato *pato);
+void dispararPato(Pato *pato);
+void estadoPato(Imagen *patoImg, Pato *pato);
+
+int main()
+{
+    srand(time(0));
+    bool fs = false;
+    Imagen *pisoImg = ventana.creaImagen("./piso.bmp");
+    Imagen *arbolImg = ventana.creaImagen("./arbol.bmp");
+    Imagen *patoImg = ventana.creaImagenConMascara("./pato1.bmp", "./patoMascara.bmp");
+    Pato *pato = (Pato *)malloc(sizeof(Pato));
+
+    if (pato == NULL)
+    {
+        printf("El pato nacio morido :'/. Error en el programa...");
+        ventana.imprimeEnConsola("El pato nacio morido :'/. Error en el programa...");
+        return 1;
+    }
+
+    pato->coorX = 600;
+    pato->coorY = 400;
+    pato->estado = 0;
+    pato->ancho = 100;
+    pato->alto = 100;
+    pato->velX = 5;
+    pato->velY = -3;
+
+    bool derecha_pres = false, izquierda_pres = false;
+    ventana.tamanioVentana(1280, 720);
+    ventana.tituloVentana("Duck Hunt");
+    ventana.colorFondo(COLORES.AZULC);
+
+    int tecla = ventana.teclaPresionada();
+
+    while (tecla != TECLAS.ESCAPE)
+    {
+
+        tecla = ventana.teclaPresionada();
+
+        if (tecla == TECLAS.ENTER)
+        {
+            fs = !fs;
+            ventana.pantallaCompleta(fs);
+        }
+
+        ventana.limpiaVentana(); // Limpiamos
+        dibujarEscenarioRes1(pisoImg, arbolImg);
+        if (ventana.anchoVentana() == 1920)
+        {
+            dibujarEscenarioRes2(pisoImg, arbolImg);
+        }
+
+        dibujarPato(patoImg, pato);
+        vueloPato(pato);
+        dispararPato(pato);
+        estadoPato(patoImg, pato);
+        ventana.actualizaVentana(); // Mostramos
+
+        int tmp = ventana.teclaSoltada();
+        if (tmp == TECLAS.DERECHA)
+        {
+            derecha_pres = false;
+        }
+        if (tmp == TECLAS.IZQUIERDA)
+        {
+            izquierda_pres = false;
+        }
+
+        ventana.espera(10);
+    }
+
+    ventana.cierraVentana();
+    free(pato);
+    return 0;
+}
+
+void dibujarEscenarioRes1(Imagen *piso, Imagen *arbol)
+{
+    ventana.muestraImagenEscalada(0, ventana.altoVentana() - 250, ventana.anchoVentana(), 350, piso);
+    ventana.muestraImagenEscalada(100, ventana.altoVentana() - 570, 150, 350, arbol);
+}
+
+void dibujarEscenarioRes2(Imagen *piso, Imagen *arbol)
+{
+    ventana.limpiaVentana(); // Limpiamos
+    ventana.muestraImagenEscalada(0, ventana.altoVentana() - 250, ventana.anchoVentana(), 350, piso);
+    ventana.muestraImagenEscalada(100, ventana.altoVentana() - 570, 150, 350, arbol);
+    ventana.actualizaVentana(); // Mostramos
+}
+
+void dibujarPato(Imagen *patoImg, Pato *pato)
+{
+    ventana.muestraImagenEscalada(pato->coorX, pato->coorY, pato->ancho, pato->alto, patoImg);
+}
+
+void vueloPato(Pato *pato)
+{
+    if (pato->estado == 0)
+    {
+        pato->coorX += pato->velX;
+        pato->coorY += pato->velY;
+    }
+
+    if (pato->coorY <= 0)
+    {
+        pato->velY *= -1;
+    }
+
+    if (pato->coorX <= 0 || (pato->coorX + pato->ancho) >= ventana.anchoVentana())
+    {
+        pato->velX *= -1;
+    }
+
+    if (pato->coorY >= (ventana.altoVentana() - 300))
+    {
+        pato->estado = 2;
+    }
+}
+
+void dispararPato(Pato *pato)
+{
+    int rx, ry;
+    bool botonPres = ventana.ratonBotonIzquierdo();
+    ventana.raton(&rx, &ry);
+    if (botonPres && pato->estado == 0)
+    {
+        if (rx >= pato->coorX &&
+            rx <= (pato->coorX + pato->ancho))
+        {
+            if (ry >= pato->coorY &&
+                ry <= (pato->coorY + pato->alto))
+            {
+                pato->estado = 1;
+                ventana.muestraMensaje1("El pato ta' morido unu", "Le diste!");
+            }
+        }
+    }
+}
+
+void estadoPato(Imagen *patoImg, Pato *pato)
+{
+    if (pato->estado == 1)
+    {
+        pato->coorY += VELOCIDAD_CAIDA;
+        dibujarPato(patoImg, pato);
+
+        if (pato->coorY >= ALTURA_DEL_PASTO)
+        {
+            pato->estado = 2;
+        }
+    }
+    else if (pato->estado == 2)
+    {
+        int maxX = ventana.anchoVentana() - pato->ancho;
+        pato->coorX = rand() % (maxX + 1);
+        pato->coorY = (rand() % (MAX_Y_APARICION - MIN_Y_APARICION + 1)) + MIN_Y_APARICION;
+        pato->velX = (rand() % 5 + 4) * ((rand() % 2 == 0) ? 1 : -1);
+        pato->velY = (rand() % 5) - 2;
+        pato->estado = 0;
+    }
+    else if (pato->estado == 0)
+    {
+        dibujarPato(patoImg, pato);
+    }
+}
