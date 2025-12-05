01aa181fd252a7c39e6daad5ca1c7b99b3680113
Jonathan Cerritos <alancomando16@gmail.com>
2025-12-05 01:01:17 -0600

    Se añaden mejoras en funcion gameLoop(), disparoPato(), se añade la struct Ronda, se añaden funcionalidades de ronda y puntaje.

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
  * @brief Juego Duck Hunt en C
  * @version 0.1
  * @date 2025-12-04
- * 
- * 
+ *
+ *
  */
 
 #include "graficos.h"
 #include <stdlib.h>
 #include <time.h>
-#define GRAVEDAD 1
 #define ALTURA_DEL_PASTO 600
 #define VELOCIDAD_CAIDA 8
 #define MIN_Y_APARICION 50
@@ -26,14 +25,21 @@ typedef struct
     int ancho;
     int velX;
     int velY;
+    int totalPatos;
 } Pato;
 
+typedef struct
+{
+    bool rondaContinuar;
+    int puntaje;
+} Ronda;
+
 void dibujarEscenarioRes1(Imagen *piso, Imagen *arbol);
-void dibujarEscenarioRes2(Imagen *piso, Imagen *arbol);
 void dibujarPato(Imagen *patoImg, Pato *pato);
 void vueloPato(Pato *pato);
-void dispararPato(Pato *pato);
+bool dispararPato(Pato *pato, Ronda *ronda);
 void estadoPato(Imagen *patoImg, Pato *pato);
+void gameLoop(Imagen *patoImg, Imagen *arbol, Imagen *piso, Pato *pato, Imagen *scoreImg, Ronda *ronda);
 
 int main()
 {
@@ -42,15 +48,14 @@ int main()
     Imagen *pisoImg = ventana.creaImagen("./piso.bmp");
     Imagen *arbolImg = ventana.creaImagen("./arbol.bmp");
     Imagen *patoImg = ventana.creaImagenConMascara("./pato1.bmp", "./patoMascara.bmp");
+    Imagen *scoreImg = ventana.creaImagenConMascara("./score.bmp", "./scoreMascara.bmp");
     Pato *pato = (Pato *)malloc(sizeof(Pato));
-
     if (pato == NULL)
     {
         printf("El pato nacio morido :'/. Error en el programa...");
         ventana.imprimeEnConsola("El pato nacio morido :'/. Error en el programa...");
         return 1;
     }
-
     pato->coorX = 600;
     pato->coorY = 400;
     pato->estado = 0;
@@ -58,51 +63,23 @@ int main()
     pato->alto = 100;
     pato->velX = 5;
     pato->velY = -3;
-
+    pato->totalPatos = 10;
+    Ronda *ronda = (Ronda *)malloc(sizeof(Ronda));
+    ronda->rondaContinuar = true;
+    ronda->puntaje = 0;
     bool derecha_pres = false, izquierda_pres = false;
     ventana.tamanioVentana(1280, 720);
     ventana.tituloVentana("Duck Hunt");
     ventana.colorFondo(COLORES.AZULC);
-
     int tecla = ventana.teclaPresionada();
-
-    while (tecla != TECLAS.ESCAPE)
+    while (tecla != TECLAS.ESCAPE && (ronda->rondaContinuar))
     {
-
         tecla = ventana.teclaPresionada();
-
-        if (tecla == TECLAS.ENTER)
-        {
-            fs = !fs;
-            ventana.pantallaCompleta(fs);
-        }
-
         ventana.limpiaVentana(); // Limpiamos
-        dibujarEscenarioRes1(pisoImg, arbolImg);
-        if (ventana.anchoVentana() == 1920)
-        {
-            dibujarEscenarioRes2(pisoImg, arbolImg);
-        }
-
-        dibujarPato(patoImg, pato);
-        vueloPato(pato);
-        dispararPato(pato);
-        estadoPato(patoImg, pato);
+        gameLoop(patoImg, arbolImg, pisoImg, pato, scoreImg, ronda);
         ventana.actualizaVentana(); // Mostramos
-
-        int tmp = ventana.teclaSoltada();
-        if (tmp == TECLAS.DERECHA)
-        {
-            derecha_pres = false;
-        }
-        if (tmp == TECLAS.IZQUIERDA)
-        {
-            izquierda_pres = false;
-        }
-
         ventana.espera(10);
     }
-
     ventana.cierraVentana();
     free(pato);
     return 0;
@@ -112,14 +89,6 @@ void dibujarEscenarioRes1(Imagen *piso, Imagen *arbol)
 {
     ventana.muestraImagenEscalada(0, ventana.altoVentana() - 250, ventana.anchoVentana(), 350, piso);
     ventana.muestraImagenEscalada(100, ventana.altoVentana() - 570, 150, 350, arbol);
-}
-
-void dibujarEscenarioRes2(Imagen *piso, Imagen *arbol)
-{
-    ventana.limpiaVentana(); // Limpiamos
-    ventana.muestraImagenEscalada(0, ventana.altoVentana() - 250, ventana.anchoVentana(), 350, piso);
-    ventana.muestraImagenEscalada(100, ventana.altoVentana() - 570, 150, 350, arbol);
-    ventana.actualizaVentana(); // Mostramos
 }
 
 void dibujarPato(Imagen *patoImg, Pato *pato)
@@ -151,7 +120,7 @@ void vueloPato(Pato *pato)
     }
 }
 
-void dispararPato(Pato *pato)
+bool dispararPato(Pato *pato, Ronda *ronda)
 {
     int rx, ry;
     bool botonPres = ventana.ratonBotonIzquierdo();
@@ -165,10 +134,44 @@ void dispararPato(Pato *pato)
                 ry <= (pato->coorY + pato->alto))
             {
                 pato->estado = 1;
+                pato->totalPatos--;
                 ventana.muestraMensaje1("El pato ta' morido unu", "Le diste!");
+                ronda->puntaje += 1000;
+                if (pato->totalPatos == 0)
+                {
+                    ronda->rondaContinuar = ventana.muestraPregunta("Terminaste el juego, felicidades! :D. Continuar?");
+                    if (ronda->rondaContinuar == false)
+                    {
+                        ventana.muestraMensaje("Hasta luego!");
+                    }
+                    else
+                    {
+                        pato->totalPatos = 10;
+                        pato->estado = 2;
+                    }
+                }
+                return false;
             }
         }
     }
+}
+
+void gameLoop(Imagen *patoImg, Imagen *arbolImg, Imagen *pisoImg, Pato *pato, Imagen *scoreImg, Ronda *ronda)
+{
+    dibujarEscenarioRes1(pisoImg, arbolImg);
+    ventana.muestraImagenEscalada(150, ventana.altoVentana() - 200, 50, 50, scoreImg);
+
+    vueloPato(pato);
+    dispararPato(pato, ronda);
+    estadoPato(patoImg, pato);
+    if (pato->estado != 2)
+    {
+        dibujarPato(patoImg, pato);
+    }
+    ventana.color(COLORES.NEGRO);
+    int rx, ry;
+    ventana.raton(&rx, &ry);
+    ventana.circuloRelleno(rx, ry, 10);
 }
 
 void estadoPato(Imagen *patoImg, Pato *pato)
@@ -176,7 +179,6 @@ void estadoPato(Imagen *patoImg, Pato *pato)
     if (pato->estado == 1)
     {
         pato->coorY += VELOCIDAD_CAIDA;
-        dibujarPato(patoImg, pato);
 
         if (pato->coorY >= ALTURA_DEL_PASTO)
         {
@@ -185,15 +187,11 @@ void estadoPato(Imagen *patoImg, Pato *pato)
     }
     else if (pato->estado == 2)
     {
-        int maxX = ventana.anchoVentana() - pato->ancho;
-        pato->coorX = rand() % (maxX + 1);
+        int maxCoorX = ventana.anchoVentana() - pato->ancho;
+        pato->coorX = rand() % (maxCoorX + 1);
         pato->coorY = (rand() % (MAX_Y_APARICION - MIN_Y_APARICION + 1)) + MIN_Y_APARICION;
         pato->velX = (rand() % 5 + 4) * ((rand() % 2 == 0) ? 1 : -1);
         pato->velY = (rand() % 5) - 2;
         pato->estado = 0;
-    }
-    else if (pato->estado == 0)
-    {
-        dibujarPato(patoImg, pato);
     }
 }
